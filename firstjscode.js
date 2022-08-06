aa1a4659089317bc75f046262d12ff2cf01a7a3d
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-08-06 17:54:35 +0530

    Create firstjscode.js

---
+++ firstjscode.js
@@ -0,0 +1,5 @@
+var username = prompt("What is your name?");
+username1 = username.slice(0,1);
+username2 = username1.toUpperCase();
+username3 = username.slice(1,name.length);
+alert("Hello " + username2 + username3);
